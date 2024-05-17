5bd9542304c6a3af48386b4de345a2ae4b0af00b
Jarosław Baran <furiousprogramming@gmail.com>
2024-05-17 21:24:23 +0200

    Added a function to test the similarity of container contents

--- Tester/TestUtils.pp
+++ Tester/TestUtils.pp
@@ -37,12 +37,22 @@ unit TestUtils;
 
 interface
 
+uses
+  Vector,
+  ListSimple,
+  ListSparse,
+  ListSparseDyn;
+
 
   function  TestGetTicks    (): Int64; inline;
 
   procedure TestPrintHeader ();
   procedure TestPrintFooter ();
   procedure TestPrintResult (AName: String; ATimeVector, ATimeSimple, ATimeSparse, ATimeSparseDyn: Int64);
+
+  {$IFDEF BUILD_DEBUG}
+  procedure TestContent     (AVector: PVector; ASimple: PListSimple; ASparse: PListSparse; ASparseDyn: PListSparseDyn);
+  {$ENDIF}
 
 
 implementation
@@ -137,6 +147,63 @@ begin
     IfThen(PlaceSparseDyn = 1, '-', PlaceSparseDyn.ToString())
   );
 end;
+
+
+{$IFDEF BUILD_DEBUG}
+
+procedure TestContent(AVector: PVector; ASimple: PListSimple; ASparse: PListSparse; ASparseDyn: PListSparseDyn);
+var
+  DataNum:       Int32;
+  DataVector:    PUInt32;
+  DataSimple:    PUInt32;
+  DataSparse:    PUInt32;
+  DataSparseDyn: PUInt32;
+  NodeSimple:    PListSimpleNode;
+  NodeSparse:    PListSparseNode;
+  NodeSparseDyn: PListSparseDynNode;
+begin
+  // Check if the number of items in the containers is the same.
+  if AVector^.DataNum <> ASimple^.NodeNum    then System.Error(reAssertionFailed);
+  if AVector^.DataNum <> ASparse^.NodeNum    then System.Error(reAssertionFailed);
+  if AVector^.DataNum <> ASparseDyn^.NodeNum then System.Error(reAssertionFailed);
+
+  // Get the number of items to check and set pointers to the first items in the containers.
+  DataNum       := AVector^.DataNum;
+  DataVector    := AVector^.Data;
+  NodeSimple    := ASimple^.NodeHead;
+  NodeSparse    := ASparse^.NodeHead;
+  NodeSparseDyn := ASparseDyn^.NodeHead;
+
+  // Iterate through the entire contents of the containers, checking each item in turn.
+  while DataNum > 0 do
+  begin
+    // Check the links in the nodes in case the integrity of the lists has been compromised.
+    if NodeSimple    = nil then System.Error(reAssertionFailed);
+    if NodeSparse    = nil then System.Error(reAssertionFailed);
+    if NodeSparseDyn = nil then System.Error(reAssertionFailed);
+
+    // Typed pointers will come in handy.
+    DataSimple    := @NodeSimple^.Data;
+    DataSparse    := @NodeSparse^.Data;
+    DataSparseDyn := @NodeSparseDyn^.Data;
+
+    // Check whether the item data in each container is the same. If not, either the filling of the containers is faulty, or
+    // something went wrong when inserting, deleting or sorting items.
+    if DataVector^ <> DataSimple^    then System.Error(reAssertionFailed);
+    if DataVector^ <> DataSparse^    then System.Error(reAssertionFailed);
+    if DataVector^ <> DataSparseDyn^ then System.Error(reAssertionFailed);
+
+    // Go to the next items.
+    DataVector    += 1;
+    NodeSimple    := NodeSimple^.Next;
+    NodeSparse    := NodeSparse^.Next;
+    NodeSparseDyn := NodeSparseDyn^.Next;
+
+    DataNum -= 1;
+  end;
+end;
+
+{$ENDIF}
 
 
 end.
