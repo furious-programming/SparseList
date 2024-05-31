1082560084d69d3fb1b1984693e0836eeffa82e7
furious programming <furiousprogramming@gmail.com>
2024-05-31 15:40:47 +0200

    Update ListSimple.pp

--- Source/ListSimple.pp
+++ Source/ListSimple.pp
@@ -1,7 +1,7 @@
 
 {
   Simple Doubly-linked List.
-  Copyleft © 2024 furious programming. All rights reversed.
+  Copyleft (ɔ) 2024 furious programming. All rights reversed.
   _______________________________________________________________________
 
   This is free and unencumbered software released into the public domain.
